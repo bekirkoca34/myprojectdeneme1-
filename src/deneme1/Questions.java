f7fb4f6f37955f0357f43c47fdc2299d019bda6c
Bekir Koca <bekirkoca8484@gmail.com>
2021-04-10 14:40:34 +0100

    d3

--- src/deneme1/Questions.java
+++ src/deneme1/Questions.java
@@ -1,12 +1,14 @@
 package deneme1;
 
+
+
 public class Questions {
 
 	public static void main(String[] args) {
 		
 		System.out.println("bekir' den merkeze");
 		System.out.println("bekir' den merkeze 2");
-		
+		System.out.println("bekir 'den merkeze 3");
 
 	}
 
